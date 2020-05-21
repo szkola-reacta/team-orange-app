16857e894c68a093c000f41a77b1adcc01dce8df
Andrzej <andrzejzmuda@hotmail.com>
2020-05-21 14:46:44 +0200

    additional one

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Assets from './components/Assets';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Assets />
     </div>
   );
 }
---
+++ src/components/Assets.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+
+class Assets extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            assetList: [],
+            errorMessage: ''
+        };
+    }
+
+    componentDidMount() {
+        const DATABASE = '/database.json';
+        axios.get(DATABASE)
+            .catch(function (error) {
+            if (error.response.status) {
+                this.setState(
+                        { errorMessage: error.response.status }
+                    )
+            }
+        })
+        .then(response => this.setState({
+                assetList: response.data.assets,
+            }))
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Assets;
