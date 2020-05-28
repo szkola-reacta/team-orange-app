69a52e93b372edbd021bebc3f17ca9c4f1ce7e08
Andrzej <andrzejzmuda@hotmail.com>
2020-05-28 14:18:10 +0200

    search field remodelled

--- src/components/AssetList.js
+++ src/components/AssetList.js
@@ -1,85 +1,37 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import DataTable, { createTheme } from 'react-data-table-component';
+import React from 'react';
+import DataTable from 'react-data-table-component';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
+import Table from 'react-bootstrap/Table';
 
 
 
 const AssetList = ({assets}) => {
 
-    const FilterComponent = ({ filterText, onFilter, onClear }) => (
-        <div>
-            <InputGroup className="mb-3">
-            <FormControl
-                id="search"
-                type="text"
-                value={filterText}
-                onChange={onFilter}
-            />
-                <InputGroup.Append>
-                <InputGroup.Text type="button" id="basic-addon2" onClick={onClear}>X</InputGroup.Text>
-                </InputGroup.Append>
-            </InputGroup>
-        </div>
-      );
-
-    console.log(assets)
-    const columns = [
-        {
-            name: 'nr śt',
-            selector: 'assetNr',
-            sortable: true,
-        },
-        {
-            name: 'nr eq',
-            selector: 'eqNr',
-            sortable: true,
-        },
-        {
-            name: 'nr seryjny',
-            selector: 'serialNumber',
-            sortable: true,
-        },
-        {
-            name: 'producent',
-            selector: 'manufacturer',
-            sortable: true,
-        },
-        {
-            name: 'opis',
-            selector: 'description',
-            sortable: true,
-        },
-    ]
-
-    const data = assets;
-    const [filterText, setFilterText] = React.useState('');
-    const [resetPaginationToggle, setResetPaginationToggle] = React.useState(false);
-    const filteredItems = data.filter(item => item.assetNr && item.assetNr.toLowerCase().includes(filterText.toLowerCase()));
-    const subHeaderComponentMemo = React.useMemo(() => {
-        const handleClear = () => {
-          if (filterText) {
-            setResetPaginationToggle(!resetPaginationToggle);
-            setFilterText('');
-          }
-        };
-        return <FilterComponent onFilter={e => setFilterText(e.target.value)} onClear={handleClear} filterText={filterText} />;
-      }, [filterText, resetPaginationToggle]);
-
     return (
-            <DataTable
-                title="Lista Środków Trwałych"
-                columns={columns}
-                data={filteredItems}
-                pagination
-                paginationResetDefaultPage={resetPaginationToggle}
-                subHeader
-                subHeaderComponent={subHeaderComponentMemo}
-                persistTableHead
-                // selectableRows
-            />
-    )
+            <Table responsive striped bordered variant="dark" hover size="sm">
+                <thead>
+                    <tr>
+                        <th>nr środka trwałego</th>
+                        <th>nr eq</th>
+                        <th>nr seryjny</th>
+                        <th>producent</th>
+                        <th>opis</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {assets.map(i =>
+                    <tr key={i.id}>
+                        <td>{i.assetNr}</td>
+                        <td>{i.eqNr}</td>
+                        <td>{i.serialNumber}</td>
+                        <td>{i.manufacturer}</td>
+                        <td>{i.description}</td>
+                    </tr>
+                    )}
+                </tbody>
+            </Table>
+        )
 }
 
 export default AssetList;
--- src/components/Assets.js
+++ src/components/Assets.js
@@ -1,42 +1,76 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Octicon, {X} from '@primer/octicons-react'
+
 import AssetList from './AssetList';
+import './Assets.css';
 
 
 class Assets extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            assetList: [],
-            errorMessage: '',
-        };
-    }
+    state = {
+        query: "",
+        data: [],
+        filteredData: []
+      };
 
-    componentDidMount() {
-        const DATABASE = '/database.json';
-        axios.get(DATABASE)
-            .catch(function (error) {
-            if (error.response.status) {
-                console.log(
-                        { errorMessage: error.message }
-                    )
-            }
-        })
-        .then(response => this.setState({
-            assetList: response.data.assets,
-            }))
-    }
+      handleInputChange = event => {
+        const query = event.target.value;
 
-    render() {
-        const {assetList, errorMessage } = this.state;
+        this.setState(prevState => {
+          const filteredData = prevState.data.filter(element => {
+            return element.assetNr.toLowerCase().includes(query.toLowerCase());
+          });
+
+          return {
+            query,
+            filteredData
+          };
+        });
+      };
+
+      handleClearInput = () => {
+          this.setState({
+              filteredData: ''
+          });
+      }
+
+      componentDidMount() {
+          const DATABASE = '/database.json';
+          axios.get(DATABASE)
+              .catch(function (error) {
+              if (error.response.status) {
+                  console.log(
+                          { errorMessage: error.message }
+                      )
+              }
+          })
+          .then(response => this.setState({
+              data: response.data.assets,
+              }))
+      }
+      render() {
         return (
             <div>
-                {assetList.length > 0 ?
-               <AssetList assets={assetList} />
-               : errorMessage}
-            </div>
-        )
-    }
-}
+                <InputGroup className="searchForm">
+                    <FormControl
+                    placeholder="Search for..."
+                    aria-label="Search for..."
+                    aria-describedby="basic-addon2"
+                    value={this.state.query}
+                    onChange={this.handleInputChange}
+                    />
+                    <InputGroup.Append>
+                    <InputGroup.Text id="basic-addon2" onClick={this.handleClearInput}>
+                        <Octicon icon={X}/>
+                    </InputGroup.Text>
+                    </InputGroup.Append>
+                </InputGroup>
+          <AssetList assets={this.state.filteredData} />
+          </div>
+        );
+      }
+  }
 
 export default Assets;
